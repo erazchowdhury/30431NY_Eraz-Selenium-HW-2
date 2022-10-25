b47142fdcb1b167aa23ad9c666d1f9b6b6557d1a
erazchowdhury <erazchow@gmail.com>
2022-10-25 01:27:24 -0400

    edited HomePage class

--- src/main/java/application/page_library/HomePage.java
+++ src/main/java/application/page_library/HomePage.java
@@ -1,9 +1,14 @@
 package application.page_library;
 
 import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BasePage {
+
+    @FindBy (xpath = "//ul[@id='homefeatured']//img[@alt='Faded Short Sleeve T-shirts']")
+    public WebElement fadedShortSleeveTShirtsImage;
 
     public HomePage () {
         PageFactory.initElements(driver, this);
