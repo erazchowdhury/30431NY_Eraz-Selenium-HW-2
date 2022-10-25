790b1a29b5bd5ff064467a7a3a847a177f795f1d
erazchowdhury <erazchow@gmail.com>
2022-10-25 00:16:27 -0400

    first push after restarting

---
+++ src/main/java/application/page_library/HomePage.java
@@ -0,0 +1,12 @@
+package application.page_library;
+
+import base.BasePage;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePage {
+
+    public HomePage () {
+        PageFactory.initElements(driver, this);
+    }
+
+}
---
+++ src/test/java/system/shopping_cart/TestShoppingCart.java
@@ -0,0 +1,10 @@
+package system.shopping_cart;
+
+import application.page_library.HomePage;
+import base.BasePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestShoppingCart extends BasePage {
+}
